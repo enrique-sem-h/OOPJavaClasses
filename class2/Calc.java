9e9cf2e6114fa1d25a5320ee6ab733f67a01e2a8
Enrique-mac <carvalhoenrique33@gmail.com>
2024-03-23 09:54:41 -0300

    exercise

--- class2/Calc.java
+++ class2/Calc.java
@@ -1,9 +1,6 @@
 package class2;
 
 public class Calc {
-    Double num1;
-    Double num2;
-
     Double sum(Double num1, Double num2){
         return num1 + num2;
     }
@@ -18,5 +15,16 @@ public class Calc {
     
     Double divide(Double num1, Double num2){
         return num1 / num2;
+    }
+    boolean isPrime(Double num) {
+    	if(num <= 1) {
+    		return false;
+    	}
+    	for(int i = 2; i <= Math.sqrt(num); i++) {
+    		if (num % i == 0) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
 }
--- class2/Main.java
+++ class2/Main.java
@@ -3,13 +3,11 @@ package class2;
 public class Main {
     public static void main(String[] args) {
         Calc calc = new Calc();
-        calc.num1 = 1.0;
-        calc.num2 = 3.4;
-        calc.sum(calc.num1, calc.num2);
 
         System.out.println("Sum: " + calc.sum(5.5, 1.2));
-        System.out.println("Sum: " + calc.sub(5.5, 1.2));
-        System.out.println("Sum: " + calc.multiply(5.0, 10.0));
-        System.out.println("Sum: " + calc.divide(5.0, 2.0));
+        System.out.println("Sub: " + calc.sub(5.5, 1.2));
+        System.out.println("Multiply: " + calc.multiply(5.0, 10.0));
+        System.out.println("Divide: " + calc.divide(5.0, 2.0));
+        System.out.println("is prime: " + calc.isPrime(6.0));
     }
 }
