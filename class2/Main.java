c7c146552957267b14aadc3f970e12a3e6047ca0
Enrique-mac <carvalhoenrique33@gmail.com>
2024-03-23 09:58:14 -0300

    exercise

--- class2/Main.java
+++ class2/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Calc calc = new Calc();
 
-        System.out.println("Sum: " + calc.sum(5.5, 1.2));
-        System.out.println("Sub: " + calc.sub(5.5, 1.2));
-        System.out.println("Multiply: " + calc.multiply(5.0, 10.0));
-        System.out.println("Divide: " + calc.divide(5.0, 2.0));
-        System.out.println("is prime: " + calc.isPrime(6.0));
+        System.out.println("Sum: " + calc.sum(2.5, 2.5));
+        System.out.println("Sub: " + calc.sub(5.5, 6.5));
+        System.out.println("Multiply: " + calc.multiply(2.0, 3.0));
+        System.out.println("Divide: " + calc.divide(6.6, 10.0));
+        System.out.println("is prime: " + calc.isPrime(0.0));
+        System.out.println("is prime: " + calc.isPrime(1.0));
+        System.out.println("is prime: " + calc.isPrime(2.0));
+        System.out.println("is prime: " + calc.isPrime(3.0));
+        System.out.println("is prime: " + calc.isPrime(7.0));
+        System.out.println("is prime: " + calc.isPrime(83.0));
+        System.out.println("is prime: " + calc.isPrime(100.0));
+        System.out.println("is prime: " + calc.isPrime(991.0));
+        System.out.println("is prime: " + calc.isPrime(104729.0));
+        System.out.println("is prime: " + calc.isPrime(14348907.0));
     }
 }
